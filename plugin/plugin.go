d74150780fea6a83c8c2aa1560289d0ea0604c05
Andrey Denisov <and.denisov26@gmail.com>
2026-02-13 19:56:45 +0300

    Add rules

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -5,12 +5,10 @@ import (
 	"golang.org/x/tools/go/analysis"
 )
 
-// AnalyzerPlugin is required by golangci-lint
 var AnalyzerPlugin analyzerPlugin
 
 type analyzerPlugin struct{}
 
-// GetAnalyzers returns the analyzers to be used by golangci-lint
 func (analyzerPlugin) GetAnalyzers() []*analysis.Analyzer {
 	return []*analysis.Analyzer{
 		linterlog.Analyzer,
